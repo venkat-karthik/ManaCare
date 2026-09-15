ccbcb72400ae94f650b93fc8394d3f6a271f26b2
Venkat Karthik <atoshi@Venkats-MacBook-Air.local>
2026-09-15 21:09:44 +0530

    shh

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,9 +4,9 @@ export const CONTACT_EMAIL = 'care@aasara.in'
 export const ADMIN_NOTIFICATION_EMAIL = 'karthikvenkat316@gmail.com'
 
 export const OFFICE_NAME = 'Villa 69, Samruddhi Green Avenues'
-export const OFFICE_LANDMARK = 'Main Campus, beside KL University'
+export const OFFICE_LANDMARK = 'beside KL University Main Campus'
 export const OFFICE_CITY_STATE = 'Mallempudi, Vijayawada Region, AP - 522303'
-export const OFFICE_FULL_ADDRESS = 'Villa 69, Samruddhi Green Avenues, Main Campus, beside KL University, Mallempudi, Nutakki (Vijayawada Region), Andhra Pradesh - 522303'
+export const OFFICE_FULL_ADDRESS = 'Villa 69, Samruddhi Green Avenues, beside KL University Main Campus, Mallempudi, Nutakki (Vijayawada Region), Andhra Pradesh - 522303'
 export const OFFICE_MAPS_URL = 'https://maps.google.com/?q=Samruddhi+Green+Avenues+KL+University+Mallempudi+Nutakki+Andhra+Pradesh+522303'
 
 
